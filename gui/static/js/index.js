36f363d71c4dad197865c9572f65a2398b3d905a
Deepam-Rai <deepmrai1@gmail.com>
2023-04-01 18:28:03 +0530

    time to work on the back-end

--- gui/static/js/index.js
+++ gui/static/js/index.js
@@ -7,12 +7,13 @@ const masterContainer = document.getElementById('master-container');
 // add event listener to the parent element
 masterContainer.addEventListener('change', function (event) {
     const targetE = event.target;
-    console.log("something changed in " + targetE.id + "  of class(es) [" + targetE.classList + "].")
     if (targetE.classList.contains("node-options")) {
         nodeSelected(targetE.id)
     }
     else if (targetE.classList.contains("property-options")) {
         propertySelected(targetE)
+        //put generate cypher button if not already present.
+        addGenerateBtn()
     }
 });
 //for clicks on buttons
@@ -21,7 +22,25 @@ masterContainer.addEventListener('click', function (event) {
     if (targetB.classList.contains("add-constraint-btn")) {
         addConstraint(targetB)
     }
+    else if (targetB.id === "generate") {
+        generate()
+    }
 })
+
+function addGenerateBtn(){
+    //adds generate CYPHER query button if not already present
+    let btnId = "generate"
+    let btn = document.getElementById(btnId)
+    if (btn){
+        return
+    }
+    btn = document.createElement("button")
+    btn.setAttribute('id',btnId)
+    btn.classList.add("generate-btn")
+    btn.textContent = "Generate CYPHER"
+    let masterContainer=document.getElementById("master-container")
+    masterContainer.appendChild(btn)
+}
 
 //ADD QUERY-CARD
 cardCount = 0;
@@ -34,29 +53,33 @@ addQueryBtn.addEventListener('click', function (event) {
     card.classList.add('query-card')
     let id = "query-card-" + cardCount
     card.setAttribute('id', id)
-    //add the card
+    //1. add the card
+    //2. add entity meta specs
+    let meta = document.createElement("div")
+    meta.classList.add("entity-meta-container")
+    meta.setAttribute('id',"entity-meta-container-"+cardCount)
+    //3.    add node options to the card-meta
+    addNodesOptions(meta)
+    card.appendChild(meta)
     field.appendChild(card);
-    //add node options to the card
-    addNodesOptions(id)
-    //add constraints-container
+    //4. add constraints-container
     let constrContainer = document.createElement("div")
     constrContainer.classList.add("constraints-container")
     constrContainer.setAttribute("id", "constraints-container-" + cardCount)
-    //add container to the card
     thisCard = document.getElementById(id)
     thisCard.appendChild(constrContainer)
     cardCount++;
 });
 
-function addNodesOptions(cardId) {
+function addNodesOptions(entityMeta) {
     let id = "node-options-" + cardCount
     //get card component
-    let card = document.getElementById(cardId)
+    // let card = document.getElementById(cardId)
     //  add label first
     let label = document.createElement("label")
     label.setAttribute('for', id)
     label.innerHTML = "Node"
-    card.appendChild(label)
+    entityMeta.appendChild(label)
     //create select list
     let seList = document.createElement('select')
     seList.classList.add('node-options')
@@ -75,15 +98,18 @@ function addNodesOptions(cardId) {
         seList.add(option)
     }
     //add the select list in card
-    card.appendChild(seList)
+    entityMeta.appendChild(seList)
 }
 
 function nodeSelected(nodeId) {
     let n = nodeId.split("-").pop()
-    //put the (+)constraint button if this is the first time changed
-    //  get the card first.
+    let qId = n;
     let cardId = "query-card-" + n
     let card = document.getElementById(cardId)
+    //add reference for the node if not exists
+    addReference(qId)
+    //put the (+)constraint button if this is the first time changed
+    //  get the card first.
     let btnId = "add-constraint-btn-" + n
     let addBtn = document.getElementById(btnId)
     if (addBtn === null) {
@@ -104,6 +130,29 @@ function nodeSelected(nodeId) {
     if (toRemove) {
         selNodes.removeChild(toRemove)
     }
+}
+function addReference(qId) {
+    //adds entiry reference used for CYPHER query to entity-meta-container
+    //if it doesnt exist already
+    let refId = "reference-" + qId
+    let ref = document.getElementById(refId)
+    if (ref) {
+        return
+    }
+    let metaField = document.getElementById("entity-meta-container-"+qId) 
+    //create label
+    let label = document.createElement("label")
+    label.setAttribute('for',refId)
+    label.innerText = 'Reference:'
+    metaField.appendChild(label)
+    //create newRef
+    let newRef = document.createElement('input')
+    newRef.setAttribute('type','text')
+    newRef.setAttribute('id',refId)
+    newRef.setAttribute('name',refId)
+    let value = "e"+qId
+    newRef.setAttribute('value',value)
+    metaField.appendChild(newRef)
 }
 
 //user clicked on +constraint button
@@ -128,7 +177,6 @@ function addConstraint(targetB) {
 }
 
 function populateProperties(constrFieldId) {
-    console.log("need to populate for " + constrFieldId)
     //first get which nodeType was selected
     let qId = constrFieldId.split('-').slice(-2, -1)[0]
     let nodeType = document.getElementById("node-options-" + qId).value
@@ -206,9 +254,12 @@ function propertySelected(propSel) {
 
     //add constraints
     appendAppropConstr(coreField, propType, qId, n)
-    console.log(constrType)
 
     //remove the default option --select property-- if it exists
+    let toRemove = propSel.querySelector(`option[value="${"null"}"]`);
+    if (toRemove) {
+        propSel.removeChild(toRemove)
+    }
 }
 function appendAppropConstr(coreField, propType, qId, n) {
     let combId = qId + "-" + n
@@ -240,7 +291,16 @@ function appendAppropConstr(coreField, propType, qId, n) {
             `;
             break;
     }
-    console.log(constrType)
     constr.innerHTML = innerHTML
     coreField.appendChild(constr)
+}
+
+
+//HANDLING submission----------------------
+function generate(){
+    //takes all the input
+    //passes to back-end
+    //waits for the response
+    //visualizes the response
+    console.info("time to work on the backend")
 }
